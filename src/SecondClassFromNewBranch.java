30798e572d707f6f6aab36b209ccd7f1d4137417
darya <varenikdarya@gmail.com>
2023-08-20 21:33:27 -0400

    updating existinf class

--- src/SecondClassFromNewBranch.java
+++ src/SecondClassFromNewBranch.java
@@ -3,6 +3,7 @@ public class SecondClassFromNewBranch {
 
 	public static void main(String[] args) {
 		System.out.println("This is change from my new branch");
+		System.out.println("This is update from my new branch");
 
 	}
 
